a7ea2e6b10ba2ad462fb41ed1ca7b88d11265955
Phil <58194823+nibofacci@users.noreply.github.com>
2022-06-01 15:19:44 +0200

    update intermediate

--- Philipp/02_Intermediate/script.js
+++ Philipp/02_Intermediate/script.js
@@ -1,3 +1,5 @@
+const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+
 const giftButtons = document.querySelectorAll(".gift-btn");
 
 const clickGift = (e) => {
@@ -6,7 +8,6 @@ const clickGift = (e) => {
 }
 
 const changeBackground = (e) => {
-    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
     const r = randomBetween(0, 127);
     const g = randomBetween(0, 127);
     const b = randomBetween(0, 127);
