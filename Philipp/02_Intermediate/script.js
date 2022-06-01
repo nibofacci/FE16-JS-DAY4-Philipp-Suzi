ad1b307ec4251c97c57724952585fcd0f37ab868
Phil <58194823+nibofacci@users.noreply.github.com>
2022-06-01 14:20:12 +0200

    add intermediate ex

---
+++ Philipp/02_Intermediate/script.js
@@ -0,0 +1,20 @@
+const giftButtons = document.querySelectorAll(".gift-btn");
+
+const clickGift = (e) => {
+    console.log(e.target.src)
+    e.target.src = "images/santa-claus.png"
+}
+
+const changeBackground = (e) => {
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 127);
+    const g = randomBetween(0, 127);
+    const b = randomBetween(0, 127);
+    const rand_rgb = `rgb(${r},${g},${b})`; // Collect all to a css color string
+    document.body.style.backgroundColor = rand_rgb
+}
+
+giftButtons.forEach(elem => {
+    elem.addEventListener("click", clickGift)
+    elem.addEventListener("click", changeBackground)
+});
